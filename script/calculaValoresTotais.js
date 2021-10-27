c9f20a89784bbbb6afe5b70961f4d702ea52daf8
Thiago Ferreira <thinwracer@gmail.com>
2021-10-27 23:15:07 +0100

    Feature Combustivel

---
+++ script/calculaValoresTotais.js
@@ -0,0 +1,13 @@
+// import { valorTotal } from "./calculoMandioca.js";
+// import { gastoCombustivel } from "./calculoCombustivel.js";
+
+function valoresTotais(){
+    console.log("Passei aqui");
+    // mandioca = valorTotal(10,10,"resultadoComCasca");
+    // combustivel = gastoCombustivel();
+    // resultado = mandioca + combustivel;
+    // document.getElementById("resultadoMandiocaCombustivel").innerHTML = resultado;
+}
+
+
+
